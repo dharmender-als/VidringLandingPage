92b8b1fc5b221091a3250e1823fe18c36c1444b0
deki08 <teamsoul234@gmail.com>
2024-05-02 00:32:42 +0530

    added servlet intializer

--- src/main/java/lp/vidring/com/VidringLandingPageApplication.java
+++ src/main/java/lp/vidring/com/VidringLandingPageApplication.java
@@ -2,8 +2,14 @@ package lp.vidring.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableWebMvc
+@EnableSpringDataWebSupport
 public class VidringLandingPageApplication {
 
 	public static void main(String[] args) {
